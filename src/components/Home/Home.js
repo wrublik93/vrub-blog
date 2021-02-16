8f531127d9e65f680b5bb3a34cbb01b4083c2ef0
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-17 00:32:34 +0300

    feat: add data for blogs and add Blog List for Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import BlogList from '../BlogList';
+
+import './Home.css';
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'My new website Ivan',
+            body: 'something....',
+            author: 'Ivan',
+            id: 1,
+        },
+        {
+            title: 'My new website Peter',
+            body: 'something....',
+            author: 'Peter',
+            id: 2,
+        },
+        {
+            title: 'My new website Lol',
+            body: 'something....',
+            author: 'Lol',
+            id: 3,
+        },
+    ]);
+
     return ( 
         <div className="home">
-            <h2 className="home-title">
-                Home page
-            </h2>
+            <BlogList blogs={blogs} title="All blogs" />
         </div>
      );
 }
