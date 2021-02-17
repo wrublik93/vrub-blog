f98f844059c78dd023e10e13514498548c506975
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-17 22:00:29 +0300

    feat: implement delete function for Blog List like prop

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,6 +1,6 @@
 import './BlogList.css';
 
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -8,10 +8,11 @@ const BlogList = ({ blogs, title }) => {
                     <div className="blog-preview" key={ blog.id }>
                         <h2 className="blog-preview-title">{ blog.title }</h2>
                         <p className="blog-preview-author">Written by: { blog.author }</p>
+                        <button onClick={ () => handleDelete(blog.id) }>Delete</button>
                     </div>
             ))}
         </div>
      );
 }
  
-export default BlogList;
+export default BlogList; 
