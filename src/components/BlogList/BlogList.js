f2f4e5de6904e6ab830cc31b032c61bd770bc460
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-18 00:20:24 +0300

    feat: implement get fetch data for all blogs

--- src/components/BlogList/BlogList.js
+++ src/components/BlogList/BlogList.js
@@ -1,6 +1,6 @@
 import './BlogList.css';
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -8,7 +8,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
                     <div className="blog-preview" key={ blog.id }>
                         <h2 className="blog-preview-title">{ blog.title }</h2>
                         <p className="blog-preview-author">Written by: { blog.author }</p>
-                        <button onClick={ () => handleDelete(blog.id) }>Delete</button>
                     </div>
             ))}
         </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,38 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from '../BlogList';
 
 import './Home.css';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'My new website Ivan',
-            body: 'something....',
-            author: 'Ivan',
-            id: 1,
-        },
-        {
-            title: 'My new website Peter',
-            body: 'something....',
-            author: 'Peter',
-            id: 2,
-        },
-        {
-            title: 'My new website Lol',
-            body: 'something....',
-            author: 'Lol',
-            id: 3,
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [isError, setIsError] = useState(null);
 
-    const handleDelete = (id) => {
+    /* const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
-    };
+    }; */
+
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error('could not fetch the data...')
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setBlogs(data);
+                setIsPending(false);
+                setIsError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setIsError(err.message);
+            });
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" handleDelete={ handleDelete }/>
+            { isError && <div>{ isError }</div> }
+            { isPending && <div>Loading...</div> }
+            { blogs && <BlogList blogs={blogs} title="All blogs" /> }
         </div>
      );
 }
