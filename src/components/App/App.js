0c263687464334b57deef37fcd043430a3cd1fbb
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-20 00:45:29 +0300

    feat: add react router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Navbar from '../Navbar';
 import Home from '../Home';
 
 import './App.css';
+import Create from '../Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
